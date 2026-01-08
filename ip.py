a949a9ca77df9fc8a5f729603e20ec58c90d7a8a
LyraxH <71412637+LyraxH@users.noreply.github.com>
2026-01-08 18:24:41 +1300

    more modes
    Added beginning code to implement more modes

--- ip.py
+++ ip.py
@@ -2,13 +2,31 @@ import json
 import requests
 import subprocess
 
-toDo = input('(g)et, (f)ind \n what do: ')
+toDo = input('(g)et, (f)ind, (s)ceure, (v)erbose \n what do: ')
 subprocess.run(["clear"])
 match (toDo):
-    case "g":
+    case "g": #prints current ip
         result = subprocess.run(['curl', 'https://api.ipify.org'], capture_output=True, text=True)
         print('your ip:',result.stdout)
     case "f":
+        toGet = input('(m)ine\ninput ip: ')
+        if toGet == 'm':
+            toGet = subprocess.run(['curl', 'https://api.ipify.org'], capture_output=True, text=True)
+            toGet = toGet.stdout
+        r = requests.get(f'http://ip-api.com/json/{toGet}')
+        data = r.json()
+        subprocess.run(["clear"])
+        print('Results:')
+        print(f'IP:  {data['query']}')
+        print(f'City:  {data['city']}')
+        print(f'Region:  {data['regionName']}')
+        print(f'Country:  {data['country']}')
+        print(f'Zip:  {data['zip']}')
+        print(f'ISP:  {data['isp']}')
+        print(f'as: {data['as']}')
+    case "s":
+        print('a');
+    case "v": #prints any ip (or yours) everything
         toGet = input('(m)ine\ninput ip: ')
         if toGet == 'm':
             toGet = subprocess.run(['curl', 'https://api.ipify.org'], capture_output=True, text=True)
